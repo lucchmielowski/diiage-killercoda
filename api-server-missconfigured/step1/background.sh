4625353a145fe4d65a7073af5011ca43b57f6794
Luc Chmielowski <luc.chmielowski@payfit.com>
2023-10-24 21:38:05 +0200

    add step to update file

--- api-server-missconfigured/step1/background.sh
+++ api-server-missconfigured/step1/background.sh
@@ -1,3 +1,7 @@
 sed -i -e 's/metadata:/metadatal;/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 sed -i -e 's/--etcd-servers=https:\/\/127.0.0.1:2379/--etcd-servers=https:\/\/127.0.0.1:23000/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 sed -i -e 's/--authorization-mode=Node,RBAC/--authorization-modus=Node,RBAC/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+
+# Reload file
+mv /etc/kubernetes/manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml.bak
+mv /etc/kubernetes/manifests/kube-apiserver.yaml.bak /etc/kubernetes/manifests/kube-apiserver.yaml
