59a57fa880b9b552e323702628faa031fceae00b
Luc Chmielowski <luc.chmielowski@payfit.com>
2023-10-24 21:35:15 +0200

    fix reversed sed

--- api-server-missconfigured/step1/background.sh
+++ api-server-missconfigured/step1/background.sh
@@ -1,3 +1,3 @@
 sed -i -e 's/metadata:/metadatal;/g' /etc/kubernetes/manifests/kube-apiserver.yaml
-sed -i -e 's/--etcd-servers=https:\/\/127.0.0.1:23000/--etcd-servers=https:\/\/127.0.0.1:2379/g' /etc/kubernetes/manifests/kube-apiserver.yaml
-sed -i -e 's/--authorization-modus=Node,RBAC/--authorization-mode=Node,RBAC/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i -e 's/--etcd-servers=https:\/\/127.0.0.1:2379/--etcd-servers=https:\/\/127.0.0.1:23000/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i -e 's/--authorization-mode=Node,RBAC/--authorization-modus=Node,RBAC/g' /etc/kubernetes/manifests/kube-apiserver.yaml
